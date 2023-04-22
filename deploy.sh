dbed100763b339d24c2713494becb9c5a0f20b29
CAIET <caiet@MacBook-Pro-de-CAIET.local>
2023-04-22 11:17:36 -0600

    gh pages added

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+ #!/usr/bin/env bash
+
+ # abort on errors
+ set -e
+
+ # build
+ yarn run build
+
+ # navigate into the build output directory
+ cd dist
+
+ # if you are deploying to a custom domain
+ # echo 'www.example.com' > CNAME
+
+ git init
+ git checkout -b main
+ git add -A
+ git commit -m 'deploy'
+
+ # if you are deploying to https://<USERNAME>.github.io
+ # git push -f git@github.com:Rodolfo-Castillo/PokedexVue3.git main:gh-pages
