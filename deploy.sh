cc10cb4ea5e7d6ca33988aea7488d0559c7f62b7
CAIET <caiet@MacBook-Pro-de-CAIET.local>
2023-04-24 12:21:03 -0600

    Cambios

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,8 @@
  git add -A
  git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+ # git push -f git@github.com:Rodolfo-Castillo/PokedexVue3.git.io.git main
+
  # if you are deploying to https://<USERNAME>.github.io
- # git push -f git@github.com:Rodolfo-Castillo/PokedexVue3.git main:gh-pages
+ # git push -f git@github.com:<USERNAME>/<USERNAME>.git main:gh-pages
